d124673699bd64659e802daef7f98fc7c8e7335d
Thomas Deruel <t.deruel@hotmail.fr>
2019-05-22 09:43:57 +0200

    insert modules

--- ANGclient/src/app/services/merchant/merchant.service.ts
+++ ANGclient/src/app/services/merchant/merchant.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class MerchantService {
 
-  constructor() { }
+  private apiUrl: string;
+
+  constructor() { 
+    this.apiUrl = `https://mcba.dwsapp.io/api/merchant/`
+  }
+
+
+  /*public authenticate = (): Promise<any> => {
+
+  }*/
+  private getData(res: any){
+    return res || {};
+  };
 }
